1112e0a631a9369ea3723fc169ed5ac1a2f9685a
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-08-29 14:41:11 -0300

    Como trabalhar com background gradient no tailwindcss de forma simples e objetiva.

---
+++ background_gradient/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primaria": "#750e49",
+        "secundaria": "#917081",
+        "terceira": "#b8d9c8"
+      }
+    },
+  },
+  plugins: [],
+}
+
